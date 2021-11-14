433517bfe497c90d4d1b0eac5c742adf8bbf510f
Anmol Parande <parandea1.7@gmail.com>
2021-11-14 11:13:42 -0800

    Add VCTK dataset for single speaker
    Will help will training on a single voice.

--- data_utils.py
+++ data_utils.py
@@ -5,6 +5,7 @@ from typing import Tuple
 from dotenv import load_dotenv
 import torch
 from torch.utils.data import Dataset
+import torchaudio
 from torchaudio.datasets import VCTK_092
 import torchaudio.transforms as transforms
 
@@ -115,3 +116,71 @@ class TTSCollate():
       output_lengths[i] = mel.size(1)
 
     return text_padded, input_lengths, mel_padded, gate_padded, output_lengths
+
+class VCTK_092_Speaker(Dataset):
+  def __init__(self, params: DataParams, speaker_id: str, root: str = DATASET_PATH, mic_id: str = "mic2", audio_ext=".flac"):
+    self.mfcc_transform = transforms.MelSpectrogram(sample_rate=params.sample_rate, n_mels=params.n_mel_channels, f_min=params.fmin, f_max=params.fmax, win_length=params.win_length, hop_length=params.hop_length, n_fft=params.filter_length)
+    self.speaker_id = speaker_id
+    if mic_id not in ["mic1", "mic2"]:
+      raise RuntimeError(f'`mic_id` has to be either "mic1" or "mic2". Found: {mic_id}')
+
+    archive = os.path.join(root, "VCTK-Corpus-0.92.zip")
+
+    self._path = os.path.join(root, "VCTK-Corpus-0.92")
+    self._txt_dir = os.path.join(self._path, "txt")
+    self._audio_dir = os.path.join(self._path, "wav48_silence_trimmed")
+    self._mic_id = mic_id
+    self._audio_ext = audio_ext
+
+    if not os.path.isdir(self._path):
+      raise RuntimeError("Dataset not found. Please use `download=True` to download it.")
+
+    # Extracting speaker IDs from the folder structure
+    self._sample_ids = []
+
+    if speaker_id == "p280" and mic_id == "mic2":
+      raise RuntimeError("No audio files")
+    utterance_dir = os.path.join(self._txt_dir, speaker_id)
+    for utterance_file in sorted(f for f in os.listdir(utterance_dir) if f.endswith(".txt")):
+      utterance_id = os.path.splitext(utterance_file)[0]
+      audio_path_mic = os.path.join(self._audio_dir, speaker_id, f"{utterance_id}_{mic_id}{self._audio_ext}")
+      if speaker_id == "p362" and not os.path.isfile(audio_path_mic):
+        continue
+      self._sample_ids.append(utterance_id.split("_")[1])
+
+  def _load_text(self, file_path) -> str:
+    with open(file_path) as file_path:
+      return file_path.readlines()[0]
+
+  def _load_audio(self, file_path) -> Tuple[torch.Tensor, int]:
+    return torchaudio.load(file_path)
+
+  def _load_sample(self, speaker_id: str, utterance_id: str, mic_id: str):
+    transcript_path = os.path.join(
+      self._txt_dir, speaker_id, f"{speaker_id}_{utterance_id}.txt"
+    )
+    audio_path = os.path.join(
+      self._audio_dir,
+      speaker_id,
+      f"{speaker_id}_{utterance_id}_{mic_id}{self._audio_ext}",
+    )
+
+    # Reading text
+    transcript = self._load_text(transcript_path)
+
+    # Reading FLAC
+    waveform, sample_rate = self._load_audio(audio_path)
+
+    return (waveform, sample_rate, transcript, speaker_id, utterance_id)
+
+  def __getitem__(self, n: int):
+    utterance_id = self._sample_ids[n]
+    waveform, sample_rate, transcript, speaker_id, utterance_id =  self._load_sample(self.speaker_id, utterance_id, self._mic_id)
+
+    mfcc = torch.log(torch.clamp(self.mfcc_transform(waveform), min=1e-5))
+    text = torch.IntTensor(text_to_sequence(transcript, ["english_cleaners"]))
+
+    return mfcc.squeeze(0), text, speaker_id, utterance_id
+
+  def __len__(self) -> int:
+      return len(self._sample_ids)
