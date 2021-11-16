49ae5608de18f5d8d4f36c59df12521a87e92722
Anmol Parande <parandea1.7@gmail.com>
2021-11-15 22:45:22 -0800

    Tacotron Implementation (#1)
    * Create initial implementation of tacotron
    
    Use Tacotron from https://github.com/NVIDIA/tacotron2
    
    * Save model more frequently
    
    * Added parameters for computing MFCCs
    
    * Load checkpoint if one exists
    
    * Enable specifying val size to train routine
    
    * Use log-Mel Spectrogram
    
    * Add VCTK dataset for single speaker
    
    Will help will training on a single voice.
    
    * Return model and loss history from train loop
    
    * Split load data from train loop
    
    * Trim start silences
    
    * Switch train to VCTK single speaker

--- data_utils.py
+++ data_utils.py
@@ -5,8 +5,13 @@ from typing import Tuple
 from dotenv import load_dotenv
 import torch
 from torch.utils.data import Dataset
+import torchaudio
 from torchaudio.datasets import VCTK_092
 import torchaudio.transforms as transforms
+import librosa
+
+from text import text_to_sequence
+from hyper_params import DataParams
 
 # Loads environment variables
 load_dotenv()
@@ -21,8 +26,8 @@ class VCTK(Dataset):
     mfcc_num: How many MFCC coeffients to use for the waveforms
     path: From where to load the dataset (defaults to DATASET_PATH from the environment)
   """
-  def __init__(self, mfcc_num: int, path: str = DATASET_PATH):
-    self.mfcc_num = mfcc_num
+  def __init__(self, params: DataParams, path: str = DATASET_PATH):
+    self.mfcc_transform = transforms.MelSpectrogram(sample_rate=params.sample_rate, n_mels=params.n_mel_channels, f_min=params.fmin, f_max=params.fmax, win_length=params.win_length, hop_length=params.hop_length, n_fft=params.filter_length)
     self.vctk = VCTK_092(path, download=True)
     self._load_speaker_metadata()
 
@@ -52,15 +57,134 @@ class VCTK(Dataset):
     """
     Load the nth sample from the dataset.
     Returns:
-      tuple: (waveform, utterance, speaker_id, utterance_id, accent, gender)
+      tuple: (MFCC, utterance, speaker_id, utterance_id, accent, gender)
     """
     sample = self.vctk[n]
     accent = self.accent_map[sample[3]]
     gender = self.gender_map[sample[3]]
-    mfcc = transforms.MFCC(sample_rate=sample[1], n_mfcc=self.mfcc_num)(sample[0])
+    
+    mfcc = torch.log(torch.clamp(self.mfcc_transform(sample[0]), min=1e-5))
+    text = torch.IntTensor(text_to_sequence(sample[2], ["english_cleaners"]))
 
-    return mfcc, *sample[2:], accent, gender
+    return mfcc.squeeze(0), text, sample[3], sample[4], accent, gender
 
   def __len__(self) -> int:
     return len(self.vctk)
 
+class TTSCollate():
+  """
+  Based on TextMelCollate from https://github.com/NVIDIA/tacotron2/blob/master/data_utils.py
+  """
+  def __init__(self, n_frames_per_step):
+    self.n_frames_per_step = n_frames_per_step
+
+  def __call__(self, batch):
+    """
+    batch: [mfcc, text, speaker_id, utterance_id, accent, gender]
+    """
+
+    # Right zero-pad all one-hot text sequences to max input length
+    input_lengths, ids_sorted_decreasing = torch.sort(
+      torch.LongTensor([len(x[1]) for x in batch]),
+      dim=0, descending=True)
+    max_input_len = input_lengths[0]
+
+    text_padded = torch.LongTensor(len(batch), max_input_len)
+    text_padded.zero_()
+    for i in range(len(ids_sorted_decreasing)):
+      text = batch[ids_sorted_decreasing[i]][1]
+      text_padded[i, :text.size(0)] = text
+
+    # Right zero-pad mel-spec
+    num_mels = batch[0][0].size(0)
+    max_target_len = max([x[0].size(1) for x in batch])
+    if max_target_len % self.n_frames_per_step != 0:
+      max_target_len += self.n_frames_per_step - max_target_len % self.n_frames_per_step
+      assert max_target_len % self.n_frames_per_step == 0
+
+    # include mel padded and gate padded
+    mel_padded = torch.FloatTensor(len(batch), num_mels, max_target_len)
+    # print(mel_padded.shape)
+    mel_padded.zero_()
+    gate_padded = torch.FloatTensor(len(batch), max_target_len)
+    gate_padded.zero_()
+    output_lengths = torch.LongTensor(len(batch))
+    for i in range(len(ids_sorted_decreasing)):
+      mel = batch[ids_sorted_decreasing[i]][0]
+      # print(mel.shape)
+      mel_padded[i, :, :mel.size(1)] = mel
+      gate_padded[i, mel.size(1)-1:] = 1
+      output_lengths[i] = mel.size(1)
+
+    return text_padded, input_lengths, mel_padded, gate_padded, output_lengths
+
+class VCTK_092_Speaker(Dataset):
+  def __init__(self, params: DataParams, speaker_id: str, root: str = DATASET_PATH, mic_id: str = "mic2", audio_ext=".flac"):
+    self.mfcc_transform = transforms.MelSpectrogram(sample_rate=params.sample_rate, n_mels=params.n_mel_channels, f_min=params.fmin, f_max=params.fmax, win_length=params.win_length, hop_length=params.hop_length, n_fft=params.filter_length)
+    self.speaker_id = speaker_id
+    if mic_id not in ["mic1", "mic2"]:
+      raise RuntimeError(f'`mic_id` has to be either "mic1" or "mic2". Found: {mic_id}')
+
+    archive = os.path.join(root, "VCTK-Corpus-0.92.zip")
+
+    self._path = os.path.join(root, "VCTK-Corpus-0.92")
+    self._txt_dir = os.path.join(self._path, "txt")
+    self._audio_dir = os.path.join(self._path, "wav48_silence_trimmed")
+    self._mic_id = mic_id
+    self._audio_ext = audio_ext
+
+    if not os.path.isdir(self._path):
+      raise RuntimeError("Dataset not found. Please use `download=True` to download it.")
+
+    # Extracting speaker IDs from the folder structure
+    self._sample_ids = []
+
+    if speaker_id == "p280" and mic_id == "mic2":
+      raise RuntimeError("No audio files")
+    utterance_dir = os.path.join(self._txt_dir, speaker_id)
+    for utterance_file in sorted(f for f in os.listdir(utterance_dir) if f.endswith(".txt")):
+      utterance_id = os.path.splitext(utterance_file)[0]
+      audio_path_mic = os.path.join(self._audio_dir, speaker_id, f"{utterance_id}_{mic_id}{self._audio_ext}")
+      if speaker_id == "p362" and not os.path.isfile(audio_path_mic):
+        continue
+      self._sample_ids.append(utterance_id.split("_")[1])
+
+  def _load_text(self, file_path) -> str:
+    with open(file_path) as file_path:
+      return file_path.readlines()[0]
+
+  def _load_audio(self, file_path) -> Tuple[torch.Tensor, int]:
+    return torchaudio.load(file_path)
+
+  def _load_sample(self, speaker_id: str, utterance_id: str, mic_id: str):
+    transcript_path = os.path.join(
+      self._txt_dir, speaker_id, f"{speaker_id}_{utterance_id}.txt"
+    )
+    audio_path = os.path.join(
+      self._audio_dir,
+      speaker_id,
+      f"{speaker_id}_{utterance_id}_{mic_id}{self._audio_ext}",
+    )
+
+    # Reading text
+    transcript = self._load_text(transcript_path)
+
+    # Reading FLAC
+    waveform, sample_rate = self._load_audio(audio_path)
+
+    return (waveform, sample_rate, transcript, speaker_id, utterance_id)
+
+  def __getitem__(self, n: int):
+    utterance_id = self._sample_ids[n]
+    waveform, sample_rate, transcript, speaker_id, utterance_id =  self._load_sample(self.speaker_id, utterance_id, self._mic_id)
+
+    _, idx = librosa.effects.trim(waveform.numpy()[0], top_db=35)
+    waveform = waveform[:, idx[0]:]
+
+    mfcc = torch.log(torch.clamp(self.mfcc_transform(waveform), min=1e-5))
+    text = torch.IntTensor(text_to_sequence(transcript, ["english_cleaners"]))
+
+    return mfcc.squeeze(0), text, waveform.squeeze(0), speaker_id, utterance_id
+
+  def __len__(self) -> int:
+      return len(self._sample_ids)
---
+++ train.py
@@ -0,0 +1,98 @@
+import os
+
+from tqdm import tqdm
+
+import torch
+from torch.utils.data import DataLoader, random_split
+
+from hyper_params import TrainingParams, TacotronParams, DataParams
+from data_utils import VCTK, TTSCollate, VCTK_092_Speaker
+from tacotron2 import Tacotron2, Tacotron2Loss
+
+def load_data(speaker:str, params: TrainingParams, data_params: DataParams, n_frames_per_step: int, val_size = 0.1):
+  dataset = VCTK_092_Speaker(data_params, speaker)
+  collate_fn = TTSCollate(n_frames_per_step)
+
+  val_size = int(val_size * len(dataset))
+  train_size = len(dataset) - val_size
+  train, val = random_split(dataset, (train_size, val_size))
+
+  train_loader = DataLoader(train, shuffle=True, batch_size=params.batch_size, drop_last=True, collate_fn=collate_fn)
+  return train_loader, val, collate_fn
+
+def validate(model, criterion, val_set, batch_size, collate_fn):
+  model.eval()
+  with torch.no_grad():
+    val_loader = DataLoader(val_set, batch_size, shuffle=False, collate_fn=collate_fn)
+
+    val_loss = 0.0
+    for i, batch in enumerate(val_loader):
+      x, y = model.parse_batch(batch)
+      y_pred = model(x)
+
+      loss = criterion(y_pred, y)
+      val_loss += loss.item()
+
+  model.train()
+  return val_loss / (i + 1)
+
+def train(params: TrainingParams, model_params: TacotronParams, data_params: DataParams, train_loader: DataLoader, val_set, collate_fn, model: Tacotron2=None, optimizer: torch.optim.Adam = None):
+  assert model_params.n_mel_channels == data_params.n_mel_channels, "MFCC output does not match data"
+
+  if model is None:
+    model = Tacotron2(model_params)
+    if os.path.exists(params.model_path):
+      print("Loading pre-trained model")
+      model.load_state_dict(torch.load(params.model_path, map_location=torch.device('cpu')))
+
+  if optimizer is None:
+    optimizer = torch.optim.Adam(model.parameters(), lr=params.learning_rate, weight_decay=params.weight_decay)
+
+  criterion = Tacotron2Loss()
+
+  if torch.cuda.is_available():
+    model = model.cuda()
+
+  iteration = 0
+  train_losses = []
+  val_losses = []
+  for epoch in range(params.epochs):
+    model.train()
+    print(f"Starting Epoch {epoch}")
+    epoch_loss = 0
+    report_loss = 0
+    for i, batch in enumerate(tqdm(train_loader)):
+      iteration += 1
+
+      model.zero_grad()
+      x, y = model.parse_batch(batch)
+      y_pred = model(x)
+
+      loss = criterion(y_pred, y)
+      loss.backward()
+
+      grad_norm = torch.nn.utils.clip_grad_norm_(model.parameters(), params.grad_clip_thresh)
+
+      optimizer.step()
+
+      epoch_loss += loss.item()
+      report_loss += loss.item()
+
+      if iteration % params.report_interval == 0:
+        print(f"Loss: {report_loss / params.report_interval}")
+        report_loss = 0
+
+      if iteration % params.save_interval == 0:
+        print(f"Saving Model")
+        torch.save(model.state_dict(), params.model_path)
+
+    val_loss = validate(model, criterion, val_set, params.batch_size, collate_fn)
+    train_loss = epoch_loss / len(train_loader)
+
+    train_losses.append(train_loss)
+    val_losses.append(val_loss)
+    print(f"Finished Epoch {epoch} with (train, val): {train_loss}, {val_loss}")
+    torch.save(model.state_dict(), params.model_path)
+
+  return model, optimizer, train_losses, val_losses
+
